e66bb705bdd9ee5eb8065b404aa3d8f260459409
The-MG <63264652+The-MG@users.noreply.github.com>
2021-06-27 12:13:48 -0700

    Update description embed

--- login.php
+++ login.php
@@ -14,9 +14,9 @@ if ($siteMaintenance == 1) {
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="PIXINVENT">
+    <meta name="description" content="Start your server with us today!">
+    <meta name="keywords" content="vex panel, vex, panel, hosting, free, paid, fast">
+    <meta name="author" content="Gallear Technologies">
     <title><?php echo $siteName ?> - Login</title>
     <link rel="apple-touch-icon" href="app-assets/images/ico/apple-icon-120.png">
     <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/favicon.ico">
